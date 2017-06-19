17624f8fb8af654d659732ec6147b5afbd6b3591
Raunaq Sawhney <raunaq.sawhney19@gmail.com>
2017-06-19 00:57:50 -0700

    Sped up animations

--- Uplift/ViewController.swift
+++ Uplift/ViewController.swift
@@ -78,7 +78,7 @@ extension ViewController : ORKTaskViewControllerDelegate {
 
 extension UIView {
 
-    func fadeIn(duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+    func fadeIn(duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
         UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
             self.alpha = 1.0
         }, completion: completion)
